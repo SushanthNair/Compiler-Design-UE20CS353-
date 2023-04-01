44863cbbf785300b2d76524fb94b1050ef4857fa
Sushanth M Nair <93919984+SushanthNair@users.noreply.github.com>
2023-04-01 11:54:48 +0530

    Delete PES1UG20CS450_CD_Lab4/Lab-4 directory

--- PES1UG20CS450_CD_Lab4/Lab-4/abstract_syntax_tree.h
+++
@@ -1,8 +0,0 @@
-typedef struct expression_node
-{
-    struct expression_node *left, *right;
-    char* value;
-}expression_node;
-
-expression_node* init_exp_node(char* val, expression_node* left, expression_node* right);
-void display_exp_tree(expression_node* exp_node);
